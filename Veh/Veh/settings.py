92fca54956ee9733bfec20176ea42223079c22fb
FiifiQwontwo <quophi555@gmail.com>
2023-07-17 13:32:56 +0000

    JWT

--- Veh/Veh/settings.py
+++ Veh/Veh/settings.py
@@ -145,6 +145,8 @@ REST_FRAMEWORK = {
 AUTH_USER_MODEL = 'accounts.Account'
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=15),
+    'ROTATE_REFRESH_TOKENS': 'True',
+    'BLACKLIST_REFRESH_TOKENS': 'True',
 }
--- Veh/accounts/urls.py
+++ Veh/accounts/urls.py
@@ -9,7 +9,7 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('register/', RegistrationView.as_view(), name='register_endpoint'),
-    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/login/refresh/', TokenRefreshView.as_view(), name='token_refresh')
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 
 ]
